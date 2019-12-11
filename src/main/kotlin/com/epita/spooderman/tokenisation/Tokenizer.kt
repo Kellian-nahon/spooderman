59849d747efdc885c2342a8254f6f71f62b28c44
kellian <43568549+backtec2@users.noreply.github.com>
2019-12-11 15:17:37 +0100

    token: put parameter in json

--- src/main/kotlin/com/epita/spooderman/tokenisation/Tokenizer.kt
+++ src/main/kotlin/com/epita/spooderman/tokenisation/Tokenizer.kt
@@ -1,18 +1,14 @@
 package com.epita.spooderman.tokenisation
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
 class Tokenizer {
 
     private var synonymHashMap: HashMap<String, String>? = null
-    private val stopWordsList = listOf("i", "me", "my", "myself", "we", "our", "ours", "ourselves",
-        "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her",
-        "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what",
-        "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been",
-        "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if",
-        "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into",
-        "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off",
-        "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any",
-        "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-        "too", "very", "s", "t", "can", "will", "just", "don", "should", "now")
+    private var delimiters: List<String>
+    private var stopWordsList: List<String>
+    private var suffixList: List<String>
 
 
     init {
@@ -27,28 +23,23 @@ class Tokenizer {
                 }
             }
         }
+
+        val mapper = jacksonObjectMapper()
+        val jsonStopWordsString = this.javaClass.getResource("/stop_words_en.json").readText()
+        stopWordsList = mapper.readValue(jsonStopWordsString)
+        val jsonDelimitersString = this.javaClass.getResource("/punctuation_delimiters_en.json").readText()
+        delimiters = mapper.readValue(jsonDelimitersString)
+        val jsonSuffixListString = this.javaClass.getResource("/suffix_en.json").readText()
+        suffixList = mapper.readValue(jsonSuffixListString)
     }
 
-    public fun getSynonymHashMap(): HashMap<String, String>? {
-        return synonymHashMap
-    }
 
-    public fun getTokens(text: String) : List<String> {
-        var doc: List<String> = text.split(' ', ',', ';', '!', '?', '.', '\'', '/', '\"')
-        return doc
-    }
-
-    public fun removeStopWord(words: List<String>): List<String> {
-        return words.filter { !stopWordsList.contains(it.toLowerCase()) }
-    }
-
-    public fun applyStemming(words: List<String>): List<String> {
-        return words.map { stemming(it) }
-    }
 
     private fun stemming(word: String): String{
-        word.removeSuffix("s")
-        word.removeSuffix("ing")
+        suffixList.forEach { suffix ->
+            if (word.endsWith(suffix, true))
+                return word.removeSuffix(suffix)
+        }
         return word
     }
 
@@ -61,12 +52,10 @@ class Tokenizer {
         return word
     }
 
-    public fun synonymousReplacement(words: List<String>): List<String> {
-        return words.map { changeToSynonyme(it) }
-    }
-
-    public fun tokenisation(text: String): List<String> {
-        var doc = text.split(' ', ',', ';', '!', '?', '.', '\'', '/', '\"')
+    fun tokenisation(text: String): List<String> {
+        var doc = text.toLowerCase()
+            .split(*delimiters.toTypedArray())
+            .filter { (it != "") }
             .filter { !stopWordsList.contains(it.toLowerCase()) }
             .map { stemming(it) }
             .map { changeToSynonyme(it) }
--- src/test/kotlin/com/epita/spooderman/TokenizerTest.kt
+++ src/test/kotlin/com/epita/spooderman/TokenizerTest.kt
@@ -7,11 +7,6 @@ import kotlin.test.assertNotEquals
 
 class TokenizerTest {
 
-    @Test
-    fun testSynonymeList() {
-        val hashMap = Tokenizer().getSynonymHashMap()
-        assertNotEquals(null, hashMap)
-    }
 
     @Test
     fun testTokenisation() {
@@ -19,10 +14,7 @@ class TokenizerTest {
         assertEquals(listOf("blue", "rabbit", "fish", "blue", "river"), tokens)
     }
 
-    @Test
-    fun testStemming() {
-        val words = Tokenizer().applyStemming(listOf("fishing", "tests"))
-        assertEquals(listOf("fish", "test"), words)
-    }
+
+
 
 }
