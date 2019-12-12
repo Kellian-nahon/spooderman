0656cff4cb86c1a66dfe8cddcc1c7b3ccfaf8928
kellian <43568549+backtec2@users.noreply.github.com>
2019-12-12 12:54:35 +0100

    Tf-Idf: Add test and compute rank after querying

--- src/main/kotlin/com/epita/spooderman/core/IdfComputer.kt
+++ src/main/kotlin/com/epita/spooderman/core/IdfComputer.kt
@@ -1,21 +1,94 @@
 package com.epita.spooderman.core
 
+import kotlin.math.acos
 import kotlin.math.ln
+import kotlin.math.log
+import kotlin.math.sqrt
+
 
 typealias TokenVector = Map<String, Pair<Float, List<Int>>>
+typealias VectorTfIdf = Map<String, Double>
 
 
 class IdfComputer(val nbDocumentInCorpus: Int) {
 
-    fun getIdf(nbDocumentInCorpus: Int, nbDocumentContainingTerm: Int): Double {
-        return ln(nbDocumentInCorpus.toDouble() / (1 + nbDocumentContainingTerm).toDouble())
+    private fun getIdf(nbDocumentContainingTerm: Int): Double {
+        return log(nbDocumentInCorpus.toDouble() / (1 + nbDocumentContainingTerm).toDouble(), 10.0)
     }
 
-    fun getVectorTfIdf(retroIndex: RetroIndex, tokens: TokenVector): List<Double> {
+    fun getVectorTfIdf(retroIndex: RetroIndex, tokens: TokenVector): VectorTfIdf {
         return tokens.map { (word, value) ->
             var nbDocumentContainingToken = retroIndex.forWord(word).count()
-            getIdf(nbDocumentInCorpus, nbDocumentContainingToken) * value.first
-        }.toList()
+            var tfIdf = getIdf(nbDocumentContainingToken) * value.first
+            Pair(word, tfIdf)
+        }.toMap()
     }
+
+    fun getCosinusSimiliraty(queryVectorTfIdf: VectorTfIdf, vectorsOfDocuments: Map<Document, VectorTfIdf>): List<Pair<Document, Double>> {
+        var listOfRank = vectorsOfDocuments.map { (doc, vector) ->
+            Pair(doc, computeSimilarity(queryVectorTfIdf, vector))
+        }.toList()
+        return listOfRank.sortedByDescending { it.second }
+
+    }
+
+    private fun computeSimilarity(queryVectorTfIdf: VectorTfIdf, documentVectorTfIdf: VectorTfIdf): Double {
+        return (dotProduct(queryVectorTfIdf, documentVectorTfIdf) / euclidianDist(queryVectorTfIdf, documentVectorTfIdf))
+    }
+
+    private fun dotProduct(vect1: VectorTfIdf, vect2: VectorTfIdf): Double {
+        var product = 0.0
+        // Loop for calculate cot product
+        vect1.forEach { (word, tfIdf) ->
+            if (vect2.containsKey(word)) {
+                val tfIdf2: Double = vect2[word] ?: 1.0
+                product += tfIdf * tfIdf2
+            }
+        }
+        return product
+    }
+
+    private fun euclidianDist(vect1: VectorTfIdf, vect2: VectorTfIdf): Double {
+        var diffSquareSum = 0.0
+        vect1.forEach { (word, tfIdf) ->
+            if (vect2.containsKey(word)) {
+                val tfIdf2: Double = vect2[word] ?: 1.0
+                diffSquareSum += (tfIdf - tfIdf2) * (tfIdf - tfIdf2)
+            }
+        }
+        return sqrt(diffSquareSum)
+    }
+
+    fun computeVectorsOfDocuments(retroIndex: RetroIndex, queryVectorTfIdf: VectorTfIdf, docs: List<Document>): Map<Document, VectorTfIdf> {
+        return docs.map { doc ->
+            Pair(doc, normalize(computeVectorOfDocument(retroIndex, queryVectorTfIdf, doc)))
+        }.toMap()
+    }
+
+    private fun computeVectorOfDocument(retroIndex: RetroIndex, queryVectorTfIdf: VectorTfIdf, doc: Document): VectorTfIdf {
+        return queryVectorTfIdf.map {(word, _) ->
+            var tfIdf = 0.0
+            if (doc.tokens.contains(word)) {
+                tfIdf = getIdf(retroIndex.forWord(word).count()) * (doc.vector[word] ?: error("")).first
+            }
+            Pair(word, tfIdf)
+        }.toMap()
+
+    }
+
+    fun normalize(vectorTfIdf: VectorTfIdf): VectorTfIdf {
+        val norme = sqrt(vectorTfIdf.map { (_, tf_idf) ->
+            tf_idf*tf_idf
+        }.sum())
+        if (vectorTfIdf.size == 1)
+            return vectorTfIdf.map { (word, _) ->
+                Pair(word, norme)
+            }.toMap()
+        return vectorTfIdf.map { (word, tf_idf) ->
+            Pair(word, (tf_idf/norme))
+        }.toMap()
+    }
+
+
 
 }
---
+++ src/test/kotlin/com/epita/spooderman/TfIdfComputerTest.kt
@@ -0,0 +1,76 @@
+package com.epita.spooderman
+
+import com.epita.spooderman.core.Document
+import com.epita.spooderman.core.IdfComputer
+import com.epita.spooderman.core.RetroIndex
+import com.epita.spooderman.tokenisation.*
+import com.epita.spooderman.vectorisation.Vectorizer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.junit.Test
+import java.util.*
+import javax.print.Doc
+import kotlin.test.assertEquals
+
+class TfIdfComputerTest {
+    
+    @Test
+    fun testRankOfDocuments() {
+        val mapper = jacksonObjectMapper()
+
+        val delimiters: List<String> = mapper.readValue(
+            Splitter::class.java.getResource("/punctuation_delimiters_en.json")
+        )
+
+        val stopWords: Set<String> = mapper.readValue(
+            StopWordReducer::class.java.getResource("/stop_words_en.json")
+        )
+
+        val suffixes: Sequence<String> = mapper.readValue(
+            StemmingReducer::class.java.getResource("/suffix_en.json")
+        )
+
+        val synonyms = hashMapOf(*mapper.readValue<Map<String, List<String>>>(
+            SynonymsReducer::class.java.getResource("/synonyms_en.json")
+        ).flatMap {(key, value) ->
+            value.map {
+                Pair(it, key)
+            }
+        }.toTypedArray())
+
+        val tokenizer = Tokenizer(
+            Splitter(delimiters),
+            listOf(
+                LowercaseReducer(), StopWordReducer(stopWords),
+                StemmingReducer(suffixes), SynonymsReducer(synonyms)
+            )
+        )
+        val vectorizer = Vectorizer()
+        val content4 = "This id a test document to avoid full matches."
+        val tokens4 = tokenizer.tokenize(content4)
+        val doc4: Document = Document(content4, tokens4, vectorizer.vectorize(tokens4))
+        val content5 = "This id another new text of document."
+        val tokens5 = tokenizer.tokenize(content5)
+        val doc5: Document = Document(content5, tokens5, vectorizer.vectorize(tokens5))
+        val content1 = "The blue rabbit is fishing in a blue river."
+        val tokens1 = tokenizer.tokenize(content1)
+        val doc1: Document = Document(content1, tokens1, vectorizer.vectorize(tokens1))
+        val content2 = "The red rabbit is cooking in a red river."
+        val tokens2 = tokenizer.tokenize(content2)
+        val doc2: Document = Document(content2, tokens2, vectorizer.vectorize(tokens2))
+        val content3 = "The green rabbit is eating in a green river."
+        val tokens3 = tokenizer.tokenize(content3)
+        val doc3: Document = Document(content3, tokens3, vectorizer.vectorize(tokens3))
+        val documents = listOf<Document>(doc1, doc2, doc3)
+        val retroIndex = RetroIndex()
+        documents.forEach { retroIndex.addDocument(it) }
+        val idfComputer = IdfComputer(3)
+        val queryVector = vectorizer.vectorize(tokenizer.tokenize("green rabbit"))
+        val queryVectorTfIdf = idfComputer.getVectorTfIdf(retroIndex, queryVector)
+        val queryVectorNormalized = idfComputer.normalize(queryVectorTfIdf)
+        val vectorsOfDocuments = idfComputer.computeVectorsOfDocuments(retroIndex, queryVectorNormalized, documents)
+        val rankList = idfComputer.getCosinusSimiliraty(queryVectorNormalized, vectorsOfDocuments)
+        print(rankList)
+    }
+    
+}
