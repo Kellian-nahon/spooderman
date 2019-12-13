69a81a5ef5e643a53f7a1e3a310fc4b968533438
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-12-13 20:49:19 +0100

    Core: Move spooderman core to core/

---
+++ core/src/main/kotlin/com/epita/spooderman/commands/ValidateUrlCommand.kt
@@ -0,0 +1,5 @@
+package com.epita.spooderman.commands
+
+import java.net.URL
+
+class ValidateUrlCommand(val url: URL)
---
+++ core/src/main/kotlin/com/epita/spooderman/events/ValidatedUrlEvent.kt
@@ -0,0 +1,5 @@
+package com.epita.spooderman.events
+
+import java.net.URL
+
+data class ValidatedUrlEvent(val url: URL)
