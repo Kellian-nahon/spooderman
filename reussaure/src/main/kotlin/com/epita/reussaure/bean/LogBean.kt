13ba8f769adba917ba4ae6cc8f2894785ed87c26
mathieu-valade <valade.mathieu@gmail.com>
2019-12-13 12:32:37 +0100

    Reussaure: Add LogBean

--- reussaure/src/main/kotlin/com/epita/reussaure/bean/LogBean.kt
+++ reussaure/src/main/kotlin/com/epita/reussaure/bean/LogBean.kt
@@ -4,7 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 interface LogBean  {
-    fun getLogger(): Logger {
+    fun logger(): Logger {
         return LoggerFactory.getLogger(this::class.java)
     }
 }
