7c3c3398b35de24900be4db2ae79d7195d9ee79e
kellian <43568549+backtec2@users.noreply.github.com>
2019-12-13 14:17:28 +0100

    SimilarityComputer: create main and controller

---
+++ victaure/src/main/kotlin/com/epita/victaure/Controller/ComputeSimilarityController.kt
@@ -0,0 +1,22 @@
+package com.epita.victaure.Controller
+
+import com.epita.victaure.core.Document
+import com.epita.victaure.core.RetroIndex
+import com.epita.victaure.core.SimilarityComputer
+import com.epita.victaure.tokenisation.Tokenizer
+import com.epita.victaure.vectorisation.Vectorizer
+import java.security.PrivateKey
+
+
+class ComputeSimilarityController(private val tokenizer: Tokenizer, private val vectorizer: Vectorizer,
+                                  private val similarityComputer: SimilarityComputer, private val retroIndex: RetroIndex) {
+
+
+    fun getDocuments(query: String): List<Document>{
+        val queryVector = vectorizer.vectorize(tokenizer.tokenize(query))
+        return similarityComputer.getDocsWithSimilarity(queryVector).map { (doc, _) -> doc }.toList()
+    }
+
+
+
+}
