67274a4eede848e60b31b462c1f5744e0628db7c
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-12-14 13:28:52 +0100

    CLI: Finish mains

--- broker/src/main/kotlin/com/epita/broker/api/server/main.kt
+++ broker/src/main/kotlin/com/epita/broker/api/server/main.kt
@@ -7,12 +7,21 @@ import com.epita.broker.broker.event_loggers.FileEventLogger
 import com.epita.reussaure.core.Reussaure
 import com.epita.reussaure.provider.Prototype
 import com.epita.reussaure.provider.Singleton
+import com.epita.spooderman.Topics
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.DefaultHelpFormatter
+import com.xenomachina.argparser.default
 import io.javalin.Javalin
 import java.io.File
 import java.util.function.Supplier
 
+class CLIArgs(parser: ArgParser) {
+    val listeningPort by parser.storing("-p", "--port", help = "The listening port for this service") {
+        toInt()
+    }.default(20001)
+}
+
 fun main(args: Array<String>) {
-    val port = args[0].toInt()
     val reussaure = Reussaure {
         val eventLogFile = File(args.getOrNull(1) ?: "event_log.txt")
         addProvider(Singleton(EventLogger::class.java, Supplier { FileEventLogger(eventLogFile.outputStream()) }))
@@ -26,5 +35,13 @@ fun main(args: Array<String>) {
         }))
     }
 
-    reussaure.instanceOf(BrokerHTTPServer::class.java).start(port)
+    ArgParser(args, helpFormatter = DefaultHelpFormatter(
+        """
+            A service use to crawl URLs.${"\n"}
+            Consuming on topic: ${Topics.ValidateURLCommand.topicId}${"\n"}
+            Producing on topic: ${Topics.ValidatedURLEvent.topicId}${"\n"}
+        """.trimIndent())
+    ).parseInto(::CLIArgs).run {
+        reussaure.instanceOf(BrokerHTTPServer::class.java).start(listeningPort)
+    }
 }
--- crawler/src/main/kotlin/com/epita/crawler/main.kt
+++ crawler/src/main/kotlin/com/epita/crawler/main.kt
@@ -12,42 +12,61 @@ import com.epita.crawler.fetcher.Fetcher
 import com.epita.crawler.urlseeker.DomUrlSeeker
 import com.epita.crawler.urlseeker.UrlSeeker
 import com.epita.reussaure.provider.Singleton
+import com.epita.spooderman.Topics
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.DefaultHelpFormatter
+import com.xenomachina.argparser.default
 import io.javalin.Javalin
 import java.net.URL
 import java.util.function.Supplier
 
+class CLIArgs(parser: ArgParser) {
+    val brokerURL by parser.storing("-u", "--url", help = "URL of the Message Broker")
+    val listeningPort by parser.storing("-p", "--port", help = "The listening port for this service") {
+        toInt()
+    }.default(20200)
+}
+
 fun main(args: Array<String>) {
+    ArgParser(args, helpFormatter = DefaultHelpFormatter(
+        """
+            A service use to crawl URLs.${"\n"}
+            Consuming on topic: ${Topics.ValidateURLCommand.topicId}${"\n"}
+            Producing on topic: ${Topics.ValidatedURLEvent.topicId}${"\n"}
+        """.trimIndent())
+    ).parseInto(::CLIArgs).run {
 
-    // TODO: Check args
-    val brokerURL = args[0]
-    val listeningPort = args[1].toInt()
+        val reussaure = Reussaure {
+            addProvider(Singleton(BrokerHTTPClient::class.java, Supplier { BrokerHTTPClient(URL(brokerURL)) }))
+            addProvider(Singleton(Javalin::class.java, Supplier { Javalin.create() }))
+            addProvider(Singleton(BrokerConsumer::class.java, Supplier {
+                BrokerConsumer(instanceOf(BrokerHTTPClient::class.java), instanceOf(Javalin::class.java))
+            }))
+            addProvider(Singleton(BrokerProducer::class.java, Supplier {
+                BrokerProducer(instanceOf(BrokerHTTPClient::class.java))
+            }))
+            addProvider(Singleton(BrokerCrawler::class.java, Supplier {
+                BrokerCrawler(
+                    instanceOf(BrokerConsumer::class.java),
+                    instanceOf(BrokerProducer::class.java),
+                    instanceOf(Crawler::class.java)
+                )
+            }))
 
-    val reussaure = Reussaure {
-        addProvider(Singleton(BrokerHTTPClient::class.java, Supplier { BrokerHTTPClient(URL(brokerURL)) }))
-        addProvider(Singleton(Javalin::class.java, Supplier { Javalin.create() }))
-        addProvider(Singleton(BrokerConsumer::class.java, Supplier {
-            BrokerConsumer(instanceOf(BrokerHTTPClient::class.java), instanceOf(Javalin::class.java))
-        }))
-        addProvider(Singleton(BrokerProducer::class.java, Supplier {
-            BrokerProducer(instanceOf(BrokerHTTPClient::class.java))
-        }))
-        addProvider(Singleton(BrokerCrawler::class.java, Supplier {
-            BrokerCrawler(instanceOf(BrokerConsumer::class.java),
-                instanceOf(BrokerProducer::class.java),
-                instanceOf(Crawler::class.java))
-        }))
+            addProvider(Singleton(Crawler::class.java, Supplier {
+                DefaultCrawler(
+                    instanceOf(Fetcher::class.java),
+                    instanceOf(Cleaner::class.java),
+                    instanceOf(UrlSeeker::class.java)
+                )
+            }))
 
-        addProvider(Singleton(Crawler::class.java, Supplier {
-            DefaultCrawler(instanceOf(Fetcher::class.java),
-                instanceOf(Cleaner::class.java),
-                instanceOf(UrlSeeker::class.java))
-        }))
+            addProvider(Singleton(Cleaner::class.java, Supplier { DomCleaner() }))
+            addProvider(Singleton(Fetcher::class.java, Supplier { DefaultFetcher() }))
+            addProvider(Singleton(UrlSeeker::class.java, Supplier { DomUrlSeeker() }))
+        }
 
-        addProvider(Singleton(Cleaner::class.java, Supplier { DomCleaner() }))
-        addProvider(Singleton(Fetcher::class.java, Supplier { DefaultFetcher() }))
-        addProvider(Singleton(UrlSeeker::class.java, Supplier { DomUrlSeeker() }))
+        val crawler = reussaure.instanceOf(BrokerCrawler::class.java)
+        crawler.start(listeningPort)
     }
-
-    val crawler = reussaure.instanceOf(BrokerCrawler::class.java)
-    crawler.start(listeningPort)
 }
