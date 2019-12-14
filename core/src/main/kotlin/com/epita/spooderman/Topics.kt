395f17749ea270c5c9eba430cb3ce50266d75561
Hugo Delahousse <hugo.delahousse@epita.fr>
2019-12-14 01:04:56 +0100

    Topics: Fix topics

--- core/src/main/kotlin/com/epita/spooderman/Topics.kt
+++ core/src/main/kotlin/com/epita/spooderman/Topics.kt
@@ -3,8 +3,11 @@ package com.epita.spooderman
 import com.epita.spooderman.types.TopicId
 
 enum class Topics(val topicId: TopicId) {
-    TO_DOCUMENTIZE_COMMAND("documentize-command"),
-    TO_INDEX_COMMAND("index-command"),
-    DOCUMENTIZED_EVENT("documentized-event")
-
+    CrawlURLCommand("crawl-url"),
+    DocumentizeContentCommand("documentize-content"),
+    IndexDocumentCommand("index-document"),
+    ValidateURLCommand("validate-url"),
+    CrawledURLEvent("crawled-url"),
+    DocumentizedContentEvent("documentized-content"),
+    ValidatedURLEvent("validated-url")
 }
