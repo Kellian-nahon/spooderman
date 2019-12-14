ee17e0d2383e0abf8b742d17c821fa9df700b5b4
mathieu-valade <valade.mathieu@gmail.com>
2019-12-14 03:02:21 +0100

    UrlValidator: add main.tkt

--- urlvalidator/src/main/kotlin/com/epita/urlvalidator/DefaultUrlValidator.kt
+++ urlvalidator/src/main/kotlin/com/epita/urlvalidator/DefaultUrlValidator.kt
@@ -1,15 +1,51 @@
 package com.epita.urlvalidator
 
+import com.epita.broker.api.client.BrokerConsumer
+import com.epita.broker.api.client.BrokerProducer
+import com.epita.broker.core.PublicationType
+import com.epita.spooderman.Topics
 import com.epita.spooderman.annotation.Mutate
+import com.epita.spooderman.commands.ValidateURLCommand
+import com.epita.spooderman.events.ValidatedURLEvent
 import com.epita.urlvalidator.core.UrlValidator
 import java.net.URL
 
-class DefaultUrlValidator() : UrlValidator {
+class DefaultUrlValidator(private val consumer: BrokerConsumer,
+                          private val producer: BrokerProducer) : UrlValidator {
     override val urlList: MutableSet<URL> = mutableSetOf()
+
+    init {
+        setup()
+    }
 
     @Synchronized
     @Mutate
     override fun validateUrl(url: URL): Boolean {
         return super.validateUrl(url)
+    }
+
+    private fun onValidateURLCommand(command: ValidateURLCommand) {
+        val isValid = validateUrl(command.url)
+
+        if (isValid) {
+            producer.sendMessage(
+                Topics.ValidatedURLEvent.topicId,
+                ValidatedURLEvent(command.url),
+                PublicationType.ONCE
+            ) { response, error ->
+                // TODO: LOG ME
+            }
+        }
+    }
+
+    private fun setup() {
+        consumer.setHandler(
+            Topics.ValidateURLCommand.topicId,
+            ValidateURLCommand::class.java
+        ) { onValidateURLCommand(it) }
+    }
+
+    fun start(serverPort: Int) {
+        consumer.start(serverPort)
     }
 }
---
+++ urlvalidator/src/main/kotlin/com/epita/urlvalidator/main.kt
@@ -0,0 +1,34 @@
+import com.epita.broker.api.client.BrokerConsumer
+import com.epita.broker.api.client.BrokerHTTPClient
+import com.epita.broker.api.client.BrokerProducer
+import com.epita.reussaure.core.Reussaure
+import com.epita.reussaure.provider.Singleton
+import com.epita.urlvalidator.DefaultUrlValidator
+import io.javalin.Javalin
+import java.net.URL
+import java.util.function.Supplier
+
+fun main(args: Array<String>) {
+
+    // TODO: Check args
+    val brokerURL = args[0]
+    val listeningPort = args[1].toInt()
+
+    val reussaure = Reussaure {
+        addProvider(Singleton(BrokerHTTPClient::class.java, Supplier { BrokerHTTPClient(URL(brokerURL)) }))
+        addProvider(Singleton(Javalin::class.java, Supplier { Javalin.create() }))
+        addProvider(Singleton(BrokerConsumer::class.java, Supplier {
+            BrokerConsumer(instanceOf(BrokerHTTPClient::class.java), instanceOf(Javalin::class.java))
+        }))
+        addProvider(Singleton(BrokerProducer::class.java, Supplier {
+            BrokerProducer(instanceOf(BrokerHTTPClient::class.java))
+        }))
+        addProvider(Singleton(DefaultUrlValidator::class.java, Supplier {
+            DefaultUrlValidator(instanceOf(BrokerConsumer::class.java), instanceOf(BrokerProducer::class.java))
+        }))
+    }
+
+    val urlValidator = reussaure.instanceOf(DefaultUrlValidator::class.java)
+    urlValidator.start(listeningPort)
+
+}
